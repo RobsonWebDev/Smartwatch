8e2c24fbc03d035da0ddb9c18b324507fcbeec98
Robson Cerqueira <84989456+RobsonWebDev@users.noreply.github.com>
2023-03-22 13:25:24 -0300

    Iniciando a Aplicacao
    Configurando a primeira parte do Projeto, como funcionara a contagem

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Smartwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void Menu(){
+            Console.WriteLine()
+        }
+
+        static void Start(int time){
+            int currentTime = 0;
+            while(currentTime != time)
+            {
+                Console.Clear();
+                currentTime ++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch Finalizado!!");
+        }
+    }
+}
