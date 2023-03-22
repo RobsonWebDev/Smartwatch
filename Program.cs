0c3843b872d8a4a7c6e10f0a67dc9661cdc000ea
Robson Cerqueira <84989456+RobsonWebDev@users.noreply.github.com>
2023-03-22 13:55:17 -0300

    Menu Criado
    Feito a Parte do menu onde o usuario vai interagir como programa iniciar o cronometro

--- Program.cs
+++ Program.cs
@@ -8,15 +8,24 @@ namespace Smartwatch
     {
         static void Main(string[] args)
         {
-            
+            Menu();
         }
 
         static void Menu(){
-            Console.WriteLine()
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1,1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
         }
 
         static void Start(int time){
             int currentTime = 0;
+
             while(currentTime != time)
             {
                 Console.Clear();
@@ -27,6 +36,7 @@ namespace Smartwatch
 
             Console.Clear();
             Console.WriteLine("Stopwatch Finalizado!!");
+            Thread.Sleep(2500);
         }
     }
 }
