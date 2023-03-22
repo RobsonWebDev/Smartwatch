a14614d8d0993288c887711cbbf98fb19dd35c59
Robson Cerqueira <84989456+RobsonWebDev@users.noreply.github.com>
2023-03-22 14:09:44 -0300

    Finalizado
    Finalizado com Sucesso feito com instruções no site balta.io do professor balta

--- Program.cs
+++ Program.cs
@@ -21,6 +21,15 @@ namespace Smartwatch
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length - 1,1));
             int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+            
+            if(time == 0)
+                System.Environment.Exit(0);
+            
+            Start(time * multiplier);
         }
 
         static void Start(int time){
@@ -37,6 +46,7 @@ namespace Smartwatch
             Console.Clear();
             Console.WriteLine("Stopwatch Finalizado!!");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
